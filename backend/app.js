7df9462eab973d073f9892847f292e340ae9ccc0
Poorna Premachandra <17600522+poorna-premachandra@users.noreply.github.com>
2025-04-15 15:53:51 +1200

    Exposed dummy APIs from backend

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: __dirname + "/.env" });
 
 var createError = require("http-errors");
 var express = require("express");
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.json({
+    message: "Demo E-Commerce API",
+    status: "Active",
+  });
 });
 
 module.exports = router;
